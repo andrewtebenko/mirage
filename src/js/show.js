a10c6bc1c50df158460f1e8f474955429400f17a
andrewtebenko <andrewtebenko@gmail.com>
2022-08-25 19:34:26 +0300

    Deleted show.js file

--- src/js/show.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    
-    // const wrapperOfCards = document.querySelector('.services__cards'),
-    //       cards = document.querySelectorAll('.services__cards-item'),
-    //       link = document.querySelectorAll('.services__cards-more');
-});
